ec539ac6e0ea9a656f6161ae34654c6841a78257
Evgenii <jenyhlevnoy@gmail.com>
2024-11-24 20:49:52 +0200

    Fixed project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("application")
-    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("org.javamodularity.moduleplugin") version "1.8.12"
     id("org.openjfx.javafxplugin") version "0.0.13"
 }
@@ -13,31 +13,37 @@ repositories {
     mavenCentral()
 }
 
-val junitVersion: String by extra("5.10.0")
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
+val junitVersion: String by extra("5.10.3")
 
 application {
     mainModule.set("com.polimerconsumer.gestures24")
     mainClass.set("com.polimerconsumer.gestures24.HelloApplication")
 }
 
-kotlin {
-    jvmToolchain(21)
-}
-
 javafx {
-    version = "21"
+    version = "17"
     modules("javafx.controls", "javafx.fxml")
 }
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    implementation("org.openjfx:javafx-controls:17")
+    implementation("org.openjfx:javafx-fxml:17")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+        options.encoding = "UTF-8"
+    }
+
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    test {
+        useJUnitPlatform()
+    }
 }
